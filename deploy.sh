f9d7f4c9f24e79cddf3d9aebad5487ad69d23f7b
lingwh <LRwq942133>
2022-09-09 01:09:04 +0800

    更新内容

--- deploy.sh
+++ deploy.sh
@@ -238,13 +238,13 @@ function deployPureCI() {
         echo '开始执行推送操作...........................'
         #git fetch https://lingwh1995:$1@github.com/lingwh1995/pure.git
         git push -f https://lingwh1995:$1@github.com/lingwh1995/pure.git HEAD:master
-        sed -i 's/base:.*,/base:\"\/\",/g' docs/.vuepress/config.ts
         #git fetch https://lingwh1995:$2@gitee.com/lingwh1995/pure.git
         echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
         ll -a
         echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
         pwd
         echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        sed -i 's/base:.*,/base:\"\/\",/g' docs/.vuepress/config.ts
         git push -f https://lingwh1995:$2@gitee.com/lingwh1995/pure.git HEAD:master
         echo '完成执行推送操作...........................'
         #回到上一次操作的目录
